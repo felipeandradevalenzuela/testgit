70f7c5d0ea2feebe50d678f1804b090c307c9889
felipeandradevalenzuela <f.andradevalenzuela@gmail.com>
2023-10-16 13:55:39 -0300

    Change on readme and new problem

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
+
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        int[] arr = new int[] {1,0,2,3,0,4,5,0};
+        Solution.duplicateZeros(arr);
     }
 }
